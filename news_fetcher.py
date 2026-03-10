59f153857e82e5044c6168609d742632af1af53a
Eternal <shashwatgupta08102008@gmail.com>
2026-03-10 20:13:28 +0530

    Add files via upload

---
+++ news_fetcher.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from datetime import datetime, timedelta
+
+
+live = datetime.now() - timedelta(days=7)
+date = live.strftime('%Y-%m-%d')
+
+queries = ["tech", "world", "india", "science"]
+
+news_list = []
+
+for query in queries:
+    nws_source = requests.get(f"https://newsapi.org/v2/everything?q={query}&from={date}&language=en&sortBy=publishedAt&apiKey=50826445f0424788a4a33312649e70fd")
+    
+    news = json.loads(nws_source.text)
+    Top_five = news["articles"][:5]
+    for article in Top_five:
+        news_list.append(article["title"])
+
+for item in news_list:
+    print(item)
